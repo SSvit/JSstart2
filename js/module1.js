5b01eaab97f3b92ac3a1dbdf6a530376923a1f34
SSvit <108158154+SSvit@users.noreply.github.com>
2023-02-13 22:00:49 +0200

    Про confirm, prompt, parseInt, parseFloat, toFixed

---
+++ js/module1.js
@@ -0,0 +1,31 @@
+// const name = 5; 
+
+// console.log('До');
+// alert('вікно');
+// console.log('Після')
+
+
+// const subscription = confirm('Продовжити підписку');
+// console.log(subscription)
+
+// let subscription = prompt('Введіть кількість товару');
+// subscription = Number(subscription);
+// console.log(subscription);
+// console.log(typeof subscription);
+
+let elrmentWidth = '50px';
+// const result = Number.parseInt(elrmentWidth);
+// console.log(result);
+
+elrmentWidth = Number.parseInt(elrmentWidth);
+
+console.log('elrmentWidth:', elrmentWidth);
+
+let elrmentHeidht = '200.74px';
+elrmentHeidht = Number.parseFloat(elrmentHeidht);
+
+console.log('elrmentHeidht:', elrmentHeidht);
+
+let salery = 1300.16472;
+salery = salery.toFixed(3);
+console.log(salery);
