cbf815fbcfccc4f3ac3b2bdc6079fc1de37a93ba
SSvit <svetlana.zaporozhenko@ukr.net>
2023-02-14 10:30:18 +0300

    Math

--- js/module1.js
+++ js/module1.js
@@ -13,19 +13,40 @@
 // console.log(subscription);
 // console.log(typeof subscription);
 
-let elrmentWidth = '50px';
+// let elrmentWidth = '50px';
 // const result = Number.parseInt(elrmentWidth);
 // console.log(result);
 
-elrmentWidth = Number.parseInt(elrmentWidth);
+// elrmentWidth = Number.parseInt(elrmentWidth);
 
-console.log('elrmentWidth:', elrmentWidth);
+// console.log('elrmentWidth:', elrmentWidth);
 
-let elrmentHeidht = '200.74px';
-elrmentHeidht = Number.parseFloat(elrmentHeidht);
+// let elrmentHeidht = '200.74px';
+// elrmentHeidht = Number.parseFloat(elrmentHeidht);
 
-console.log('elrmentHeidht:', elrmentHeidht);
+// console.log('elrmentHeidht:', elrmentHeidht);
 
-let salery = 1300.16472;
-salery = salery.toFixed(3);
-console.log(salery);
+// let salery = 1300.16472;
+// salery = salery.toFixed(3);
+// console.log(salery);
+
+let quantity = '30';
+let value = 'Цей рядок неможливо привести  до числа';
+
+// console.log(Number(false));
+// console.log(Number(true));
+// console.log(Number('5'));
+// console.log(Number('jhhjkl'));
+// console.log(Number('value'));
+
+
+// console.log(Math.PI);
+
+// const base = 2;
+// const power = 5;
+
+// // const result = Math.pow(2, 3);
+// const result = Math.pow(base, power);
+// //приведення до степеня
+
+// console.log(result);
