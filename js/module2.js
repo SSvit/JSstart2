c0fad70d059911414eb3aac36232254c7627df25
SSvit <svetlana.zaporozhenko@ukr.net>
2023-02-22 12:44:03 +0300

    Масив - складний тип

--- js/module2.js
+++ js/module2.js
@@ -1,9 +1,27 @@
-const friends = ['Mango', 'Kivi', 'Poly', 'Ajax'];
-console.table(friends);
+// const friends = ['Mango', 'Kivi', 'Poly', 'Ajax'];
+// console.table(friends);
 
-friends[1] = 'kjhgfcvbn';
+// friends[1] = 'kjhgfcvbn';
 
-console.table(friends[2]);
+// console.table(friends[2]);
 
-const lastIndex = friends.length - 1;
-console.log(lastIndex);
+// const lastIndex = friends.length - 1;
+// consola 
+
+
+// const a = [1, 2, 3];
+// const b = a;
+
+// console.log('a', a);
+// console.log('b', b);
+
+// a[0] = 500;
+
+// console.log('a', a);
+// console.log('b', b);
+
+// console.log(a === b); 
+// // a дорівнює b
+
+// console.log([1, 2, 3] === [1, 2, 3]);
+// //а не дорівнює b, тому що це посилання, складний тип
