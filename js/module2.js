cd61a0338b45177b0508b83f26c7fd3f8319bcb8
SSvit <svetlana.zaporozhenko@ukr.net>
2023-02-22 12:18:20 +0300

    Про масив

---
+++ js/module2.js
@@ -0,0 +1,9 @@
+// const friends = ['Mango', 'Kivi', 'Poly', 'Ajax'];
+// console.log(friends);
+
+// friends[1] = 'kjhgfcvbn';
+
+// console.log(friends[2]);
+
+// const lastIndex = friends.length - 1;
+// console.log(lastIndex);
