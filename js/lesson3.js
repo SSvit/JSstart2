ddc12bfd113bf90b61f520d01ec73dc4062e37d9
SSvit <svetlana.zaporozhenko@ukr.net>
2023-02-28 12:41:00 +0300

    Урок 3 задача 2

--- js/lesson3.js
+++ js/lesson3.js
@@ -1,23 +1,42 @@
-//Пробний СВІЙ варіант
-const genres = ['Jazz', 'Blues'];
-const newGenres = 'Рок-н-ролл';
+// //Пробний СВІЙ варіант
+// const genres = ['Jazz', 'Blues'];
+// const newGenres = 'Рок-н-ролл';
 
-console.log(genres);
+// console.log(genres);
 
-genres.splice(2, 0, newGenres);
-console.log(genres);
+// genres.splice(2, 0, newGenres);
+// console.log(genres);
 
-console.log(genres.slice(0, 1));
+// console.log(genres.slice(0, 1));
 
-console.log(genres.slice(-1));
+// console.log(genres.slice(-1));
 
-console.log(genres.splice(0, 1));
+// console.log(genres.splice(0, 1));
 
-const firstGenres = 'Country';
-const twoGenres = 'Reggae';
+// const firstGenres = 'Country';
+// const twoGenres = 'Reggae';
 
-genres.splice(0, 0, firstGenres);
-console.log(genres);
+// genres.splice(0, 0, firstGenres);
+// console.log(genres);
 
-genres.splice(1, 0, twoGenres);
-console.log(genres);
+// genres.splice(1, 0, twoGenres);
+// console.log(genres);
+
+
+// //задача 2
+// const values = '8 11';
+// console.log(values);
+
+// const arr = values.split(' ');
+// console.log(arr);
+
+// деструктуризація
+// const [width, heidht] = arr;
+
+// // const width = arr[0];
+// // const heidht = arr[1];
+// // console.log(width);
+// // console.log(heidht);
+
+// const area = Number(width) * Number(heidht);
+// console.log(area);
